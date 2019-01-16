2110bb939de2d7513dbe724c925923f603ae5a51
Narwanto <narwanto@nostratech.com>
2019-01-16 11:27:36 +0700

    add counter service

---
+++ counterservice/src/main/java/com/narwa/springboot_microservice/counterservice/CounterserviceApplication.java
@@ -0,0 +1,14 @@
+package com.narwa.springboot_microservice.counterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterserviceApplication.class, args);
+	}
+
+}
+
