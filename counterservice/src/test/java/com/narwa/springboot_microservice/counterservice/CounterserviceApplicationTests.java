11c743bcfa0897f2c330d03d986bc3e4a6d36d66
Narwanto <narwanto@nostratech.com>
2019-01-16 11:45:41 +0700

    add testcase with junit

--- counterservice/src/test/java/com/narwa/springboot_microservice/counterservice/CounterserviceApplicationTests.java
+++ counterservice/src/test/java/com/narwa/springboot_microservice/counterservice/CounterserviceApplicationTests.java
@@ -2,16 +2,35 @@ package com.narwa.springboot_microservice.counterservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
 public class CounterserviceApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void testCount() throws Exception {
+		ResponseEntity<String> entity = restTemplate
+				.getForEntity("http://localhost:" + this.port + "/count", String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
+
 
 }
 
