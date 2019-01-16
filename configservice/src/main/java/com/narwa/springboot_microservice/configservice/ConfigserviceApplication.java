5401b59a155eff7ede4fbca33d84ff8db1c4c39e
Narwanto <narwanto@nostratech.com>
2019-01-16 14:11:32 +0700

    add configservice

---
+++ configservice/src/main/java/com/narwa/springboot_microservice/configservice/ConfigserviceApplication.java
@@ -0,0 +1,15 @@
+package com.narwa.springboot_microservice.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableConfigServer
+@SpringBootApplication
+public class ConfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserviceApplication.class, args);
+	}
+
+}
+
